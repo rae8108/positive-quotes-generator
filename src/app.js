73e86d884b18f708095f35f4aed4cfcda3fa711e
rae8108 <146367231+rae8108@users.noreply.github.com>
2023-12-19 17:00:58 +0000

    Added basic form handling and added Typewriter plugin

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
+function generateQuote(event) {
+  event.preventDefault();
 
+  new Typewriter("#quote", {
+    strings: ["Hello", "World"],
+    autoStart: true,
+    delay: 1,
+    cursor: null,
+  });
+}
+
+let quotesFormElement = document.querySelector("#positive-quotes-form");
+quotesFormElement.addEventListener("submit", generateQuote);
